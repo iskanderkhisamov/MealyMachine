9fc959c9737a31d361133e3320144eb7d5abd6d3
i.khisamov <i.khisamov@bars-open.ru>
2022-09-08 15:06:47 +0300

    hello

--- Program.cs
+++ Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿State ChangeState(State current, Input input) => (current, input) switch
+{
+    (State.закрыта, Input.Открыть) => State.открыта,
+    (State.открыта, Input.Закрыть) => State.закрыта,
+    (State.закрыта, Input.Запереть) => State.заперта,
+    (State.заперта, Input.Отпереть) => State.закрыта,
+    _ => throw new NotSupportedException(
+        $"Состояние {current} не имеет перехода {input}")
+};
+
+var inputValues = Enum.GetValues(typeof(Input));
+var stateValues = Enum.GetValues(typeof(State));
+
+var random = new Random();
+var randomState = (State)stateValues.GetValue(random.Next(stateValues.Length));
+
+var current = randomState;
+
+var count = 1;
+var stats = stateValues.Cast<object?>().ToDictionary(stateValue => stateValue.ToString(), inputValue => 0);
+
+try
+{
+    while (true)
+    {
+        var randomInput = (Input)inputValues.GetValue(random.Next(inputValues.Length));
+        stats[current.ToString()]++;
+        Console.Write($"ИТЕРАЦИЯ: {count}; ВХОДНОЙ СИГНАЛ: {randomInput} дверь; ТЕКУЩЕЕ СОСТОЯНИЕ: дверь {current}; ");
+        current = ChangeState(current, randomInput);
+        Console.WriteLine($"ВЫХОДНОЙ СИГНАЛ: дверь {current}");
+        count++;
+    }
+}
+catch (NotSupportedException e)
+{
+    Console.WriteLine($"\n\n{e.Message}");
+}
+
+Console.WriteLine($"\nСтатистика");
+Console.WriteLine(count);
+foreach (var keyValuePair in stats)
+{
+    float proc = (int)Math.Round((double)(100 * keyValuePair.Value) / count);
+    Console.WriteLine($"Дверь {keyValuePair.Key}: {keyValuePair.Value} раз ({proc}% времени)");
+}
+
+// 3 состояния (m,p)
+internal enum State { открыта, закрыта, заперта }
+
+// 4 перехода (n)
+internal enum Input { Открыть, Закрыть, Запереть, Отпереть }
